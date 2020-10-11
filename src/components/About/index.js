2bd0540f48f4544fd9910bb3dcc41ccfdf1e103b
jack <biffisbuffm@gmail.com>
2020-10-11 18:10:19 -0500

    fixed about bio spacing by creating divs

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -23,11 +23,14 @@ function About() {
                     <p>
                         I'm a full-stack web developer with a background in social media, advocacy work,
                         sociology, and management.
-                    <br></br>
+                    </p>
+                    <p className="about-bio">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    <br></br>
+                    </p>
+                    <p className="about-bio">
                         Location: Madison, WI
                     </p>
+                    
                     </div>
                 </div>
 
