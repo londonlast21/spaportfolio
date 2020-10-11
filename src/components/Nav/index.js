d737a71ff63448ba94c9cfae24df5b21f6cdf42a
jack <biffisbuffm@gmail.com>
2020-10-11 16:05:44 -0500

    edit

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,63 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 function Nav() {
-
-    // const categories = [
-    //     {
-    //         name: "Portfolio",
-    //         description: "Check out some of my work"
-    //     },
-    //     {
-    //         name: "Resume",
-    //         description: "Current resume"
-    //     }
-    // ]
-
-    // function categorySelected(name) {
-    //     console.log(`${name} clicked`)
-    // }
-
-
-  return (
-    <header>
-        <h2>
-            <a href="/">
-                <span role="img" className="my-name">J. L. Cady🌱</span> 
-            </a>
-        </h2>
-        <nav>
-            <ul className="flex-row">
-                <li className="mx-2">
-                    <span>About Me</span>
-                </li>
-
-                <li className="mx-2">
-                    <span>Portfolio</span>
-                </li>
-        
-                <li className="mx-2">
-                    <span>Contact</span>
-                </li>
-
-                <li className="mx-2">
-                    <span>Resume</span>
-                </li>
-
-                    {/* {categories.map((category) => (
-                        <li 
-                            className="mx-1"
-                            key={category.name}
-                            >
-                                <span onClick={() => categorySelected(category.name)} >
-                                    {category.name}
-                                </span>
-                            </li>
-
-                    ))} */}
-            </ul>
-        </nav>
-    </header>
-  );
+const [currentCategory, setCurrentCategory] = useState(categories[0]);
+const [categories] = useState([
+    {
+        name: 'About Me',
+    },
+    {
+        name: 'Portfolio',
+    },
+    {
+        name: 'Contact',
+    },
+    {
+        name: 'Resume'
+    }
+])
+    
 }
 
 export default Nav;
