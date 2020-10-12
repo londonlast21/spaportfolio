9e102d512af94fc602637104601df50523c0eb36
jack <biffisbuffm@gmail.com>
2020-10-11 20:39:07 -0500

    Updates

---
+++ precache-manifest.6e1233021890e3c71fd92169e6d6cc4c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12a004f42df48f3a0a3cc5e52707a0d3",
+    "url": "/spaportfolio/index.html"
+  },
+  {
+    "revision": "73ab66af9ea5865267a0",
+    "url": "/spaportfolio/static/css/main.83e1772d.chunk.css"
+  },
+  {
+    "revision": "2d0cd35463a6a8405934",
+    "url": "/spaportfolio/static/js/2.fcecb94d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spaportfolio/static/js/2.fcecb94d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73ab66af9ea5865267a0",
+    "url": "/spaportfolio/static/js/main.981a0a24.chunk.js"
+  },
+  {
+    "revision": "503c44decb5f021afb2a",
+    "url": "/spaportfolio/static/js/runtime-main.1276cc48.js"
+  },
+  {
+    "revision": "cdcce04d44e71adab930d764b0e4fcf2",
+    "url": "/spaportfolio/static/media/budgettracker.cdcce04d.png"
+  },
+  {
+    "revision": "369a6f794fda9f59f6f474f293c5b71f",
+    "url": "/spaportfolio/static/media/password.369a6f79.png"
+  },
+  {
+    "revision": "7e497a9b6a96f9446b2c63580d57125f",
+    "url": "/spaportfolio/static/media/profile.7e497a9b.png"
+  },
+  {
+    "revision": "db50cead5013284dc61377c7e1cae177",
+    "url": "/spaportfolio/static/media/quiz.db50cead.png"
+  },
+  {
+    "revision": "91356656c48ebb83f3cf1f5cb6c97e4d",
+    "url": "/spaportfolio/static/media/run-buddy.91356656.png"
+  },
+  {
+    "revision": "1cdc744d2ef7f443c87b1e36220acf22",
+    "url": "/spaportfolio/static/media/screenshot.1cdc744d.png"
+  },
+  {
+    "revision": "3cbabb1adb82d4330c0ac7c6a58f46f0",
+    "url": "/spaportfolio/static/media/transforproj.3cbabb1a.png"
+  }
+]);
