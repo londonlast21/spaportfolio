711ccba87c24815cc8745603d49be6aeab8165ea
jack <biffisbuffm@gmail.com>
2020-10-11 19:22:08 -0500

    fixed color on portfolio links, removed text dec

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -26,7 +26,7 @@ function Portfolio() {
                      </p>
                      <img src={projectImg1} className="work-img" style={{ width: "100%" }} alt="Run-Buddy Application" />
                     </a>
-                    <a href="https://github.com/londonlast21/run-buddy">GitHub Repo</a>
+                    <a className="link-port" href="https://github.com/londonlast21/run-buddy">GitHub Repo</a>
                     </div>
 
                     <div className="work-grid-item">
@@ -37,7 +37,7 @@ function Portfolio() {
                         <img src={projectImg2} className="my-2" style={{ width: "100%" }} alt="Trans Safe-Healthcare App" />
                         
                         </a>
-                        <a href="https://github.com/londonlast21/project2">GitHub Repo</a>
+                        <a className="link-port" href="https://github.com/londonlast21/project2">GitHub Repo</a>
                     </div>
                     
 
@@ -48,7 +48,7 @@ function Portfolio() {
                         </p>
                         <img src={projectImg3} className="my-2" style={{ width: "100%" }} alt="Password Generator" />
                         </a>
-                        <a href="https://github.com/londonlast21/password">GitHub Repo</a>
+                        <a className="link-port" href="https://github.com/londonlast21/password">GitHub Repo</a>
 
                     </div>
 
@@ -62,7 +62,7 @@ function Portfolio() {
                         </p>
                         <img src={projectImg4} className="mx-2" style={{ width: "100%" }} alt="Timed Coding Quiz" />
                         </a>
-                        <a href="https://github.com/londonlast21/quiz">GitHub Repo</a>
+                        <a className="link-port" href="https://github.com/londonlast21/quiz">GitHub Repo</a>
                     </div>
 
                     <div className="work-grid-item">
@@ -72,7 +72,7 @@ function Portfolio() {
                         </p>
                         <img src={projectImg5} className="mx-2" style={{ width: "100%" }} alt="On and offline budget tracker app" />
                         </a>
-                        <a href="https://github.com/londonlast21/budgetapp">GitHub Repo</a>
+                        <a className="link-port" href="https://github.com/londonlast21/budgetapp">GitHub Repo</a>
                     </div>
 
                     <div className="work-grid-item">
@@ -83,7 +83,7 @@ function Portfolio() {
                             </p>
                             <img src={projectImg6} className="mx-2" style={{ width: "100%" }} alt="Tracking app for mental health recovery" />
                         </a>
-                        <a href="https://github.com/londonlast21/filmskeleton">GitHub Repo</a>
+                        <a className="link-port" href="https://github.com/londonlast21/filmskeleton">GitHub Repo</a>
                     </div>
 
                     </div>
