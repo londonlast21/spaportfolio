08d4366b104349b71a9212719d78bdaa14e8ff3c
jack <biffisbuffm@gmail.com>
2020-10-11 22:04:52 -0500

    edit

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -20,7 +20,7 @@ function About() {
                     <img src={coverImage} className="bio-pic" style={{ width: "25%" }} alt="cover" />
                     <div className="about-column-block">
                     <h3 className="me-name">Jack L. Cady</h3>
-                    <p className="tagline">
+                    <p className="about-bio">
                         I'm a full-stack web developer with a background in social media, advocacy work,
                         sociology, and management.
                     </p>
